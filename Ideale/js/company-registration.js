83b719642bf85a28368c02efe10a4c80edc80d49
Jorge-Marin <52929850+Jorge-Marin@users.noreply.github.com>
2019-08-17 02:31:29 -0600

    Barras de Navegacion para empresas

--- Ideale/js/company-registration.js
+++ Ideale/js/company-registration.js
@@ -4,7 +4,7 @@ function initMap(){
        center: {lat:42.3601, lng:-71.0589}
    }
 
-   var map = new google.maps.Map(document.getElementById('map'), options);
+   var mapCompany = new google.maps.Map(document.getElementById('map'), options);
 }
 
 function putBannerBackground(evt) {
@@ -19,6 +19,7 @@ function putBannerBackground(evt) {
       }
 
       var reader = new FileReader();
+      document.getElementById('div-message-publicity').style.display = 'none';
 
       if(i==0){
         $('#ol-indicators').append(`
@@ -109,6 +110,3 @@ $(document).ready(function () {
 
 document.getElementById('file-banner').addEventListener('change', putBannerBackground, false);
 document.getElementById('file-logotype').addEventListener('change', putLogotypeBackground, false);
-
-/*
-*/
