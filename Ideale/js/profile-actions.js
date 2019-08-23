ddb704c3d5034f8332f28432c4a2729f23f6b881
Jorge-Marin <52929850+Jorge-Marin@users.noreply.github.com>
2019-08-23 08:49:31 -0600

    Cambios

---
+++ Ideale/js/profile-actions.js
@@ -0,0 +1,18 @@
+function activeCurrent(currentActive){
+    var navElements = ['home','products','promotions','logotype','locals','dashboard','promocion'];
+
+    for(let i=0; i<navElements.length; i++){
+        if($('#'+navElements[i]).hasClass('active')){
+            $('#'+navElements[i]).removeClass('active');
+            $('#'+navElements[i]).remove('span');
+            $('#'+navElements[i]+'-a').removeClass('border-active');            
+        }
+    } 
+    
+
+    $('#'+currentActive).addClass('active');
+    $('#'+currentActive).append(
+        `<span class="sr-only">(current)</span>`  
+    );
+    $('#'+currentActive+' a').addClass('border-active');
+}
